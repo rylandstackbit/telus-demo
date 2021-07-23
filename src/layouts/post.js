f3bdb218ae5332caea325adee9b70b3ba7fb8b13
Stackbit Code Editor <projects@stackbit.com>
2021-07-23 13:57:29 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -21,7 +21,9 @@ export default function Post(props) {
                 <div className="inner-medium">
                     <article className="post post-full">
                         <header className="post-header">
-                            {(title === "lorem-ipsum")}
+                            { (title === "lorem-ipsum") ? 
+
+                            }
                             <h1 className="post-title">{title}</h1>
                             {subtitle && <div className="post-subtitle">{subtitle}</div>}
                         </header>
